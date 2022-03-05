e5aacd1bc0062b37ddbada62688a9dee68c8f573
cristiansuzuki <cristiansuzuki5@hotmail.com>
2022-03-05 18:46:01 -0300

    removing button component

--- src/components/Button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonGenerator, ButtonGeneratorText } from "./styles";
-
-export default function Button() {
-  return (
-    <ButtonGenerator>
-      <ButtonGeneratorText>Gerar senha</ButtonGeneratorText>
-    </ButtonGenerator>
-  );
-}
